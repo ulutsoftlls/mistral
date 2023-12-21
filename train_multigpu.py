102abb223fb19fad8b89d2535666b74d4a75ffff
ulutsoftlls <ulutsoftlls@gmail.com>
2023-12-21 16:04:05 +0600

    train_multigpu.py and train_with_new_tag.py added

---
+++ train_multigpu.py
@@ -0,0 +1,149 @@
+from datasets import load_dataset
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, Trainer, TrainingArguments, DataCollatorForLanguageModeling
+from peft import prepare_model_for_kbit_training
+from peft import LoraConfig, get_peft_model
+import wandb, os
+from datetime import datetime
+
+#data
+train_dataset = load_dataset('json', data_files='./data/all_data/train.jsonl', split='train')
+eval_dataset = load_dataset('json', data_files='./data/all_data/validation.jsonl', split='train')
+
+
+def formatting_func(example):
+    text = f"<s>[KYR] {example['input']} [/KYR] {example['output']} </s>"
+    return text
+
+base_model_id = "mistralai/Mistral-7B-v0.1"
+
+model = AutoModelForCausalLM.from_pretrained(
+	base_model_id,
+	torch_dtype=torch.bfloat16,
+        device_map="auto",
+        trust_remote_code=True
+        )
+
+tokenizer = AutoTokenizer.from_pretrained(
+    base_model_id,
+    padding_side="left",
+    add_eos_token=True,
+    add_bos_token=True,
+    torch_dtype=torch.bfloat16,
+    use_fast=True, trust_remote_code=True
+)
+tokenizer.pad_token = tokenizer.eos_token
+
+def generate_and_tokenize_prompt(prompt):
+    return tokenizer(formatting_func(prompt))
+    
+
+tokenized_train_dataset = train_dataset.map(generate_and_tokenize_prompt)
+tokenized_val_dataset = eval_dataset.map(generate_and_tokenize_prompt)
+
+max_length = 2048 # This was an appropriate max length for my dataset
+
+def generate_and_tokenize_prompt2(prompt):
+    result = tokenizer(
+        formatting_func(prompt),
+        truncation=True,
+        max_length=max_length,
+        padding="max_length",
+    )
+    result["labels"] = result["input_ids"].copy()
+    return result
+
+
+tokenized_train_dataset = train_dataset.map(generate_and_tokenize_prompt2)
+tokenized_val_dataset = eval_dataset.map(generate_and_tokenize_prompt2)
+
+model.gradient_checkpointing_enable()
+model = prepare_model_for_kbit_training(model)
+
+def print_trainable_parameters(model):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    print(
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+    )
+
+
+config = LoraConfig(
+    r=64,
+    lora_alpha=128,
+    target_modules=[
+        "q_proj",
+        "k_proj",
+        "v_proj",
+        "o_proj",
+        "gate_proj",
+        "up_proj",
+        "down_proj",
+        "lm_head",
+    ],
+    bias="none",
+    lora_dropout=0.05,  # Conventional
+    task_type="CAUSAL_LM",
+)
+
+model = get_peft_model(model, config)
+print_trainable_parameters(model)
+
+
+from accelerate import FullyShardedDataParallelPlugin, Accelerator
+from torch.distributed.fsdp.fully_sharded_data_parallel import FullOptimStateDictConfig, FullStateDictConfig
+
+fsdp_plugin = FullyShardedDataParallelPlugin(
+    state_dict_config=FullStateDictConfig(offload_to_cpu=True, rank0_only=False),
+    optim_state_dict_config=FullOptimStateDictConfig(offload_to_cpu=True, rank0_only=False),
+)
+
+accelerator = Accelerator(fsdp_plugin=fsdp_plugin)
+
+model = accelerator.prepare_model(model)
+project = "train_old"
+base_model_name = "mistral"
+run_name = base_model_name + "-" + project
+output_dir = "./" + run_name
+
+trainer = Trainer(
+    model=model,
+    train_dataset=tokenized_train_dataset,
+    eval_dataset=tokenized_val_dataset,
+    args=TrainingArguments(
+        output_dir=output_dir,
+        warmup_steps=1,
+        per_device_train_batch_size=4,
+        gradient_accumulation_steps=1,
+        max_steps=60000,
+        learning_rate=1e-5, # Want a small lr for finetuning
+        fp16=True,
+        optim="paged_adamw_32bit",
+        logging_steps=6000,              # When to start reporting loss
+        logging_dir="./logs",        # Directory for storing logs
+        save_strategy="steps",       # Save the model checkpoint every logging step
+        save_steps=6000,                # Save checkpoints every 50 steps
+        evaluation_strategy="steps", # Evaluate the model every logging step
+        eval_steps=6000,               # Evaluate and save checkpoints every 50 steps
+        do_eval=True,                # Perform evaluation at the end of training
+        run_name=f"{run_name}-{datetime.now().strftime('%Y-%m-%d-%H-%M')}"          # Name of the W&B run (optional)
+    ),
+    data_collator=DataCollatorForLanguageModeling(tokenizer, mlm=False),
+)
+
+model.config.use_cache = False  # silence the warnings. Please re-enable for inference!
+
+for name, module in trainer.model.named_modules():
+    if "norm" in name:
+        module = module.to(torch.float32)
+
+trainer.train()
+
+
---
+++ train_with_new_tag.py
@@ -0,0 +1,145 @@
+from datasets import load_dataset
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+from peft import prepare_model_for_kbit_training
+from peft import LoraConfig, get_peft_model
+import wandb, os
+
+#data
+train_dataset = load_dataset('json', data_files='./data/all_data/train.jsonl', split='train')
+eval_dataset = load_dataset('json', data_files='./data/all_data/validation.jsonl', split='train')
+
+
+def formatting_func(example):
+    text = f"<s>[KYR] {example['input']} [/KYR] {example['output']}</s>"
+    return text
+    
+    
+
+base_model_id = "mistralai/Mistral-7B-v0.1"
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16
+)
+
+model = AutoModelForCausalLM.from_pretrained(base_model_id, quantization_config=bnb_config)
+
+tokenizer = AutoTokenizer.from_pretrained(
+    base_model_id,
+    padding_side="left",
+    add_eos_token=True,
+    add_bos_token=True,
+)
+tokenizer.pad_token = tokenizer.eos_token
+
+def generate_and_tokenize_prompt(prompt):
+    return tokenizer(formatting_func(prompt))
+    
+
+tokenized_train_dataset = train_dataset.map(generate_and_tokenize_prompt)
+tokenized_val_dataset = eval_dataset.map(generate_and_tokenize_prompt)
+
+max_length = 2048 # This was an appropriate max length for my dataset
+
+def generate_and_tokenize_prompt2(prompt):
+    result = tokenizer(
+        formatting_func(prompt),
+        truncation=True,
+        max_length=max_length,
+        padding="max_length",
+    )
+    result["labels"] = result["input_ids"].copy()
+    return result
+
+
+tokenized_train_dataset = train_dataset.map(generate_and_tokenize_prompt2)
+tokenized_val_dataset = eval_dataset.map(generate_and_tokenize_prompt2)
+
+model.gradient_checkpointing_enable()
+model = prepare_model_for_kbit_training(model)
+
+def print_trainable_parameters(model):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    print(
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+    )
+
+
+config = LoraConfig(
+    r=64,
+    lora_alpha=128,
+    target_modules=[
+        "q_proj",
+        "k_proj",
+        "v_proj",
+        "o_proj",
+        "gate_proj",
+        "up_proj",
+        "down_proj",
+        "lm_head",
+    ],
+    bias="none",
+    lora_dropout=0.05,  # Conventional
+    task_type="CAUSAL_LM",
+)
+
+model = get_peft_model(model, config)
+print_trainable_parameters(model)
+
+wandb.login()
+wandb_project = "train_with_new_tag"
+if len(wandb_project) > 0:
+    os.environ["WANDB_PROJECT"] = wandb_project
+
+if torch.cuda.device_count() > 1: # If more than 1 GPU
+    model.is_parallelizable = True
+    model.model_parallel = True
+    
+import transformers
+from datetime import datetime
+
+project = "train_with_new_tag"
+base_model_name = "mistral"
+run_name = base_model_name + "-" + project
+output_dir = "./" + run_name
+
+trainer = transformers.Trainer(
+    model=model,
+    train_dataset=tokenized_train_dataset,
+    eval_dataset=tokenized_val_dataset,
+    args=transformers.TrainingArguments(
+        output_dir=output_dir,
+        warmup_steps=1,
+        per_device_train_batch_size=2,
+        gradient_accumulation_steps=1,
+        max_steps=300000,
+        learning_rate=1e-5, # Want a small lr for finetuning
+        bf16=True,
+        optim="paged_adamw_8bit",
+        logging_steps=50000,              # When to start reporting loss
+        logging_dir="./logs",        # Directory for storing logs
+        save_strategy="steps",       # Save the model checkpoint every logging step
+        save_steps=50000,                # Save checkpoints every 50 steps
+        evaluation_strategy="steps", # Evaluate the model every logging step
+        eval_steps=50000,               # Evaluate and save checkpoints every 50 steps
+        do_eval=True,                # Perform evaluation at the end of training
+        report_to="wandb",           # Comment this out if you don't want to use weights & baises
+        run_name=f"{run_name}-{datetime.now().strftime('%Y-%m-%d-%H-%M')}"          # Name of the W&B run (optional)
+    ),
+    data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False),
+)
+
+model.config.use_cache = False  # silence the warnings. Please re-enable for inference!
+trainer.train()
+
+
